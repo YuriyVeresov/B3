aff6f80635f3c5d9dabc65d65389bd9c932537b1
YuriyVeresov <yuriyveresov@yandex.ru>
2021-04-20 16:34:17 +0900

    New implementation

--- src/main/java/com/javacourse/App.java
+++ src/main/java/com/javacourse/App.java
@@ -1,8 +1,23 @@
 package com.javacourse;
 
 
+import com.javacourse.stack.ArrayStack;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        ArrayStack<Integer> stack = new ArrayStack<>();
+        stack.push(2);
+        stack.push(3);
+        stack.push(0);
+        stack.push(1);
+        stack.push(10);
+        stack.push(3);
+        stack.push(2);
+        stack.push(10);
+        stack.push(0);
+
+        System.out.println(stack);
     }
 }
